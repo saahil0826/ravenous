9dd2625b2262b7c62ada39572114806676cd30e9
saahil0826 <snawalkha0826@gmail.com>
2020-07-20 23:17:57 +0530

    changes made to .gitignore

---
+++ build/precache-manifest.86b963cecd9c8c773f54297bac4afad8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2806781c57b62120c8168c7b39b4b36f",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "30afd1ce9d178a8fe97f",
+    "url": "/ravenous/static/css/main.f5d0f1ae.chunk.css"
+  },
+  {
+    "revision": "35badcdaec3a472354c5",
+    "url": "/ravenous/static/js/2.8702d92f.chunk.js"
+  },
+  {
+    "revision": "82e2acb4bd55f5de284d06bde85616ed",
+    "url": "/ravenous/static/js/2.8702d92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30afd1ce9d178a8fe97f",
+    "url": "/ravenous/static/js/main.ef58ae48.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
